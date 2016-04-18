8d50baa4f185967dca4ca12efca3fafa10312aa1
Aaron Crickenberger <spiffxp@gmail.com>
2016-04-18 12:34:08 -0700

    Use more verbose ansible output by default

--- setup.sh
+++ setup.sh
@@ -15,5 +15,5 @@ echo 'Installing local python requirements'
 pip install -r requirements.txt
 
 echo 'Running ansible'
-ansible-playbook -i ansible/inventory.local ansible/jenkins-up.yaml
-ansible-playbook -i ansible/inventory.remote --ssh-common-args '-o StrictHostKeyChecking=no' ansible/jenkins.yaml
+ansible-playbook -vv --diff -i ansible/inventory.local ansible/jenkins-up.yaml
+ansible-playbook -vv --diff --ssh-common-args '-o StrictHostKeyChecking=no' -i ansible/inventory.remote ansible/jenkins.yaml
