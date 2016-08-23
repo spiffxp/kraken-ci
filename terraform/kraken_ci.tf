2869734b2ce02004b0dc4258f40063923428aabd
Aaron Crickenberger <spiffxp@gmail.com>
2016-08-23 16:11:56 -0700

    terraform renaming
    rename pipelet.tf to kraken_ci.tf
    s/jenkins/kraken_ci except for the ssh-related params
    s/pipelet/kraken_ci

--- terraform/kraken_ci.tf
+++ terraform/kraken_ci.tf
@@ -27,7 +27,7 @@ resource "template_file" "ansible_inventory" {
   template = "${file("${path.module}/templates/inventory.remote.tpl")}"
 
   vars {
-    public_ip = "${aws_instance.jenkins_ec2.public_ip}"
+    public_ip = "${aws_instance.kraken_ci_ec2.public_ip}"
     private_jenkins_ssh_key = "${var.private_jenkins_ssh_key}"
   }
 
@@ -36,7 +36,7 @@ resource "template_file" "ansible_inventory" {
   }
 }
 
-resource "aws_vpc" "jenkins_vpc" {
+resource "aws_vpc" "kraken_ci_vpc" {
   cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = true
@@ -47,7 +47,7 @@ resource "aws_vpc" "jenkins_vpc" {
   }
 }
 
-resource "aws_vpc_dhcp_options" "jenkins_vpc_dhcp" {
+resource "aws_vpc_dhcp_options" "kraken_ci_vpc_dhcp" {
   domain_name         = "${var.aws_region}.compute.internal"
   domain_name_servers = ["AmazonProvidedDNS"]
 
@@ -56,25 +56,25 @@ resource "aws_vpc_dhcp_options" "jenkins_vpc_dhcp" {
   }
 }
 
-resource "aws_vpc_dhcp_options_association" "jenkins_vpc_dhcp_association" {
-  vpc_id          = "${aws_vpc.jenkins_vpc.id}"
-  dhcp_options_id = "${aws_vpc_dhcp_options.jenkins_vpc_dhcp.id}"
+resource "aws_vpc_dhcp_options_association" "kraken_ci_vpc_dhcp_association" {
+  vpc_id          = "${aws_vpc.kraken_ci_vpc.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.kraken_ci_vpc_dhcp.id}"
 }
 
-resource "aws_internet_gateway" "jenkins_vpc_gateway" {
-  vpc_id = "${aws_vpc.jenkins_vpc.id}"
+resource "aws_internet_gateway" "kraken_ci_vpc_gateway" {
+  vpc_id = "${aws_vpc.kraken_ci_vpc.id}"
 
   tags {
     Name = "${var.kraken_ci_hostname}_gateway"
   }
 }
 
-resource "aws_route_table" "jenkins_vpc_rt" {
-  vpc_id = "${aws_vpc.jenkins_vpc.id}"
+resource "aws_route_table" "kraken_ci_vpc_rt" {
+  vpc_id = "${aws_vpc.kraken_ci_vpc.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.jenkins_vpc_gateway.id}"
+    gateway_id = "${aws_internet_gateway.kraken_ci_vpc_gateway.id}"
   }
 
   tags {
@@ -82,8 +82,8 @@ resource "aws_route_table" "jenkins_vpc_rt" {
   }
 }
 
-resource "aws_network_acl" "jenkins_vpc_acl" {
-  vpc_id = "${aws_vpc.jenkins_vpc.id}"
+resource "aws_network_acl" "kraken_ci_vpc_acl" {
+  vpc_id = "${aws_vpc.kraken_ci_vpc.id}"
 
   egress {
     protocol   = "-1"
@@ -108,13 +108,13 @@ resource "aws_network_acl" "jenkins_vpc_acl" {
   }
 }
 
-resource "aws_key_pair" "jenkins_keypair" {
+resource "aws_key_pair" "kraken_ci_keypair" {
   key_name = "${var.aws_key_name}"
   public_key = "${file("${var.jenkins_ssh_key}")}"
 }
 
-resource "aws_subnet" "jenkins_subnet" {
-  vpc_id = "${aws_vpc.jenkins_vpc.id}"
+resource "aws_subnet" "kraken_ci_subnet" {
+  vpc_id = "${aws_vpc.kraken_ci_vpc.id}"
   cidr_block = "10.0.0.0/22"
   map_public_ip_on_launch = true
 
@@ -123,15 +123,15 @@ resource "aws_subnet" "jenkins_subnet" {
   }
 }
 
-resource "aws_route_table_association" "jenkins_subnet_rt_association" {
-  subnet_id      = "${aws_subnet.jenkins_subnet.id}"
-  route_table_id = "${aws_route_table.jenkins_vpc_rt.id}"
+resource "aws_route_table_association" "kraken_ci_subnet_rt_association" {
+  subnet_id      = "${aws_subnet.kraken_ci_subnet.id}"
+  route_table_id = "${aws_route_table.kraken_ci_vpc_rt.id}"
 }
 
-resource "aws_security_group" "jenkins_secgroup" {
+resource "aws_security_group" "kraken_ci_secgroup" {
   name = "${var.aws_secgroup_name}"
   description = "Security group for ${var.kraken_ci_hostname} Jenkins server"
-  vpc_id = "${aws_vpc.jenkins_vpc.id}"
+  vpc_id = "${aws_vpc.kraken_ci_vpc.id}"
 
   ingress {
     from_port = 22
@@ -166,13 +166,13 @@ resource "aws_security_group" "jenkins_secgroup" {
   }
 }
 
-resource "aws_instance" "jenkins_ec2" {
+resource "aws_instance" "kraken_ci_ec2" {
   depends_on = ["template_file.cloudconfig"]
   ami = "${coreosbox_ami.coreos_ami.box_string}"
   instance_type = "${var.aws_instance_type}"
-  key_name = "${aws_key_pair.jenkins_keypair.key_name}"
-  vpc_security_group_ids = [ "${aws_security_group.jenkins_secgroup.id}" ]
-  subnet_id = "${aws_subnet.jenkins_subnet.id}"
+  key_name = "${aws_key_pair.kraken_ci_keypair.key_name}"
+  vpc_security_group_ids = [ "${aws_security_group.kraken_ci_secgroup.id}" ]
+  subnet_id = "${aws_subnet.kraken_ci_subnet.id}"
   associate_public_ip_address = true
   ebs_block_device {
     device_name = "/dev/sdf"
@@ -184,16 +184,16 @@ resource "aws_instance" "jenkins_ec2" {
   }
 }
 
-resource "aws_route53_record" "pipelet_route" {
-  depends_on = ["aws_instance.jenkins_ec2", "template_file.ansible_inventory"]
+resource "aws_route53_record" "kraken_ci_route" {
+  depends_on = ["aws_instance.kraken_ci_ec2", "template_file.ansible_inventory"]
   zone_id = "${var.route53_zone_id}"
   name = "${var.kraken_ci_hostname}"
   type = "A"
   ttl = "30"
-  records = ["${aws_instance.jenkins_ec2.public_ip}"]
+  records = ["${aws_instance.kraken_ci_ec2.public_ip}"]
 }
 
-resource "aws_s3_bucket" "pipelet_clusters_bucket" {
+resource "aws_s3_bucket" "kraken_ci_clusters_bucket" {
   bucket = "${var.clusters_bucket}"
   acl = "private"
 }
